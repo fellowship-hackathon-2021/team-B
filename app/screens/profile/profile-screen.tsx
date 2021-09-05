21de2ca39859bce52279e115374f581b51e0bd50
muhamrif <muhammad.hamza6415@gmail.com>
2021-09-05 03:12:50 -0400

    Update profile-screen.tsx

--- app/screens/profile/profile-screen.tsx
+++ app/screens/profile/profile-screen.tsx
@@ -4,12 +4,23 @@ import { ViewStyle } from "react-native"
 import { Screen, Text } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
-import { color } from "../../theme"
+import {
+  Button,
+  Header,
+  Screen,
+  Text,
+  GradientBackground,
+  AutoImage as Image,
+} from "../../components"
+import { color, spacing, typography } from "../../theme"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
 }
+
+
+
 
 export const ProfileScreen = observer(function ProfileScreen() {
   // Pull in one of our MST stores
