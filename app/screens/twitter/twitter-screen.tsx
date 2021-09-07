3828d96224ea0ffba25049c065b8569f0b87e61c
ar3441 <ar3441@nyu.edu>
2021-09-07 12:54:16 -0700

    twitter screen ui

--- app/screens/twitter/twitter-screen.tsx
+++ app/screens/twitter/twitter-screen.tsx
@@ -1,25 +1,54 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { Screen, Text } from "../../components"
+import { TextInput, ViewStyle, StyleSheet, Keyboard } from "react-native"
+import { Screen, Text, TextField, Button} from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
 
-const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
-  flex: 1,
-}
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: color.palette.offWhite
 
-export const TwitterScreen = observer(function TwitterScreen() {
+  },
+  button: {
+    margin: 30,
+    backgroundColor: color.palette.black,
+    borderWidth: 1,
+    height:40,
+    
+  },
+
+
+  });
+
+export const TwitterScreen = function TwitterScreen() {
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  const [text, onChangeText] = React.useState("Enter Hashtag or Keyword...");
+ 
+
   return (
-    <Screen style={ROOT} preset="scroll">
-      <Text preset="header" text="twitter API stuff goes in this screen" />
+    <Screen style={styles.input} preset="scroll">
+      {/* <Text preset="header" text="twitter API stuff goes in this screen" /> */}
+      <TextInput
+        onChangeText = {onChangeText}
+        value = {text}
+        onSubmitEditing={Keyboard.dismiss}
+      /> 
+
+      <Button mode = "contianed" style={styles.button}>
+          <Text> Find Tweets </Text>
+      </Button>
+      
+
     </Screen>
   )
-})
+}
