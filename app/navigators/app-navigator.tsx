71289e2e414fa1d7561ec65481c7d777315172b2
david <david.zhao.9991@gmail.com>
2021-09-08 14:44:55 -0400

    fixed header

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -31,7 +31,7 @@ const AppStack = () => {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: true,
+        headerShown: false,
       }}
       initialRouteName="lock"
     >
